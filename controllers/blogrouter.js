355c39863d15651bc2d24a966d3c87c86d359c60
BasilBinoy <bailbinoy1401@gmail.com>
2024-02-09 12:04:55 +0530

    signup

--- controllers/blogrouter.js
+++ controllers/blogrouter.js
@@ -2,14 +2,32 @@ const express=require("express")
 const router=express.Router()
 
 const blogmodels=require("../models/blogmodels")
+const bcrypt=require("bcryptjs")
 
 
+hashPasswordGenerator=async(pass)=>{
+const salt=await  bcrypt.genSalt(10)
+return bcrypt.hash(pass,salt)
+}
 router.post("/signup",async(req,res)=>{
-    let data=req.body
-    let blog =new blogmodels(data)
-    let result=await blog.save()
-    res.json({
-        status:"success"
+
+
+    let {data}={"data":req.body}
+    let password =password.data
+    hashPasswordGenerator(password).then(
+        (hashedpassword)=>{
+            console.log(hashedpassword)
+            data.password=hashedpassword
+            console.log(data)
+            // let user=new userModel(data)
+            // let result=user.save()
+            res.json({
+                status:"success"
+            })
+        }
+    )
+   
     })
-})
+
+
 module.exports=router
